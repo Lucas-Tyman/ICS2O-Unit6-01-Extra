78c564769c495d82a1d1d5db279517ffb901da73
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-17 19:45:37 +0000

    2 changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+'use strict'
+
 /**
  * Check service worker.
  */
@@ -12,8 +14,15 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function calculates 
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() { 
+  // input
+  const radius = parseFloat(document.getElementById('radius-of-sphere').value)
+
+  // process
+  const volume = 4/3 * Math.PI * Math.pow(radius, 3)
+  
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
 }
